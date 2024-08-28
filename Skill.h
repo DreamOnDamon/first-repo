4aaac66d683432659e551dd8aa49c4036490b3ed
Damon_Zheng <z_s_jie@hotmail.com>
2024-08-28 15:26:07 +0800

    Modified function Skill_01

--- Skill.h
+++ Skill.h
@@ -3,5 +3,13 @@ void NormalAttack () {
 }
 
 void Skill_01 () {
+    int distance_Goal{0.2f};
+}
 
+void Skill_02(){
+    int distance_Goal{0.4f};
+}
+
+void Skill_Ult(){
+    int distance_Goal{0.7f};
 }
